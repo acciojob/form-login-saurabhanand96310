61f927683e1618104ab9b204c01087465f5ad219
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-02-23 15:06:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue(e) {
     //Write your code here
-	e.preventDefault();
+	//e.preventDefault();
      let first=document.querySelectorAll("input");
 	let f=first[0].value;
 	let last=first[1].value;

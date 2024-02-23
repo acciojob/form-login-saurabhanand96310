24d49bbceabfdda35d14365c20c3f7e642bbeec7
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-02-23 14:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
+function getFormvalue(e) {
     //Write your code here
+	e.preventDefault();
+     let first=document.querySelectorAll("input");
+	let f=first[0].value;
+	let last=first[1].value;
 
+		alert(f+" "+last);
 }
